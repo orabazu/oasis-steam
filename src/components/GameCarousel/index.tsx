5652ccea5078587f0f2cfaff948382c0cfff6b50
zhunor <ozerorhun@gmail.com>
2022-04-17 22:04:26 +0300

    Add images

---
+++ src/components/GameCarousel/index.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable jsx-a11y/alt-text */
+import './GameCarousel.scss';
+
+// import { ChromeOutlined } from '@ant-design/icons';
+import { Col, Row } from 'antd';
+import Text from 'antd/lib/typography/Text';
+import Title from 'antd/lib/typography/Title';
+import img1 from 'assets/sd1.jpg';
+import img2 from 'assets/sd2.jpg';
+import img3 from 'assets/sd3.jpg';
+import img4 from 'assets/sd4.jpg';
+import React, { useState } from 'react';
+
+export const GameCarousel = () => {
+  const [mainImage, setMainImage] = useState(img1);
+  return (
+    <Row className="GameCarousel">
+      <Col span={18} className="GameCarouselLeft">
+        <img src={mainImage} className="GameImage"></img>
+      </Col>
+      <Col span={6} className="GameCarouselRight">
+        <Text type="secondary">Top Played</Text>
+        <Title level={2} style={{ margin: `0 0 20px 0` }}>
+          Stardew Valley
+        </Title>
+        <Row gutter={[16, 24]}>
+          <Col span={12}>
+            <img
+              src={img2}
+              className="GameImage imageThumb"
+              onMouseOver={() => setMainImage(img2)}
+              onFocus={() => setMainImage(img2)}
+            ></img>
+          </Col>
+          <Col span={12}>
+            <img
+              src={img3}
+              className="GameImage imageThumb"
+              onMouseOver={() => setMainImage(img3)}
+              onFocus={() => setMainImage(img3)}
+            ></img>
+          </Col>
+          <Col span={12}>
+            <img
+              src={img4}
+              className="GameImage imageThumb"
+              onMouseOver={() => setMainImage(img4)}
+              onFocus={() => setMainImage(img4)}
+            ></img>
+          </Col>
+          <Col span={12}>
+            <img
+              src={img1}
+              className="GameImage imageThumb"
+              onMouseOver={() => setMainImage(img1)}
+              onFocus={() => setMainImage(img1)}
+            ></img>
+          </Col>
+        </Row>
+        <Row>
+          <Text style={{ marginTop: 10 }}>
+            Play from your browser, earn up to 5$ a day
+          </Text>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
---
+++ src/components/RightBar/index.tsx
@@ -0,0 +1,10 @@
+import { GameCarousel } from 'components/GameCarousel';
+import React from 'react';
+
+export const RightBar = () => {
+  return (
+    <div>
+      <GameCarousel></GameCarousel>
+    </div>
+  );
+};
