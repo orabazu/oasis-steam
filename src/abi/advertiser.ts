ffb24f751abebf8e9fffc1b1bf7f966f5627477c
zhunor <ozerorhun@gmail.com>
2022-04-21 23:18:58 +0300

    Add advertiser token

---
+++ src/abi/advertiser.ts
@@ -0,0 +1,4 @@
+export const advertiserAddress = '0x7790b8300123a497f3E48d871DdF483dCB8d5722';
+export const advertiserABI = [
+  'function mintNFT(string calldata _metadataURI ) external payable',
+];
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,45 @@
 import './Footer.scss';
 
 import { PlusOutlined, SmileFilled } from '@ant-design/icons';
+import { advertiserABI, advertiserAddress } from 'abi/advertiser';
 import { Button } from 'antd';
 import Text from 'antd/lib/typography/Text';
+import { ethers } from 'ethers';
 import React from 'react';
 
 export const Footer = () => {
+  const mintAdvertiserToken = async () => {
+    try {
+      const { ethereum } = window;
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(
+        advertiserAddress,
+        advertiserABI,
+        signer,
+      );
+
+      // connectedContract.mintNFT('put ipfs link later on').then((res: any) => {
+      //   console.log(res);
+      // });
+
+      let nftTxn = await connectedContract.mintNFT('put ipfs link later on', {
+        value: ethers.utils.parseEther('1.0'),
+      });
+
+      await nftTxn.wait();
+
+      console.log(nftTxn);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="Footer">
-      <Button icon={<PlusOutlined />} type="primary">
-        Add a Game
+      <Button icon={<PlusOutlined />} type="primary" onClick={mintAdvertiserToken}>
+        Call contract
       </Button>
       <Text>2022 © Amazing Team Name</Text>
       <Button type="text" icon={<SmileFilled />}>
