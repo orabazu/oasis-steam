453fbe04f25a0240b66c6e5b7960cd7c625c46ad
zhunor <ozerorhun@gmail.com>
2022-04-22 01:48:26 +0300

    Add games to carousel

--- src/components/GameCarousel/index.tsx
+++ src/components/GameCarousel/index.tsx
@@ -5,120 +5,125 @@ import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { Button, Col, Row } from 'antd';
 import Text from 'antd/lib/typography/Text';
 import Title from 'antd/lib/typography/Title';
-import img1 from 'assets/sd1.jpg';
-import img2 from 'assets/sd2.jpg';
-import img3 from 'assets/sd3.jpg';
-import img4 from 'assets/sd4.jpg';
-import React, { useState } from 'react';
+import aImg1 from 'assets/aim-trainer/aimg1.png';
+import aImg2 from 'assets/aim-trainer/aimg2.png';
+import aImg3 from 'assets/aim-trainer/aimg3.png';
+import mgImg1 from 'assets/memory-game/img1.png';
+import mgImg2 from 'assets/memory-game/img2.png';
+import mgImg3 from 'assets/memory-game/img3.png';
+import dsImg1 from 'assets/sdw/sd1.jpg';
+import dsImg2 from 'assets/sdw/sd2.jpg';
+import dsImg3 from 'assets/sdw/sd3.jpg';
+import dsImg4 from 'assets/sdw/sd4.jpg';
+import wImg1 from 'assets/whell-fortune/wimg1.png';
+import wImg2 from 'assets/whell-fortune/wimg2.png';
+import wImg3 from 'assets/whell-fortune/wimg3.png';
+import wImg4 from 'assets/whell-fortune/wimg4.png';
+import React, { useEffect, useState } from 'react';
+
+const games = [
+  {
+    category: 'Top Played',
+    name: 'Stardew Valley',
+    description: 'Play from your browser, earn up to 5$ a day',
+    heroImage: dsImg1,
+    images: [dsImg1, dsImg2, dsImg3, dsImg4],
+    link: '',
+  },
+  {
+    category: 'Top Played',
+    name: 'Memory Game',
+    description: 'Try to remember memory matrix',
+    heroImage: mgImg1,
+    images: [mgImg1, mgImg2, mgImg3],
+    link: 'https://memory-game-xi-two.vercel.app/',
+  },
+  {
+    category: 'Top Played',
+    name: 'Aim training',
+    description: 'Aim trainer, get better at your favorite FPS game 🏹',
+    heroImage: aImg1,
+    images: [aImg1, aImg2, aImg3],
+    link: 'https://demo-aim-game-e0ae5ejdt-tunahandanis.vercel.app/',
+  },
+  {
+    category: 'Top Played',
+    name: 'Whell of Fortune',
+    description: 'Fingers crossed 🤞',
+    heroImage: wImg1,
+    images: [wImg1, wImg2, wImg3, wImg4],
+    link: 'https://wheel-of-fortune-573k7fiws-tunahandanis.vercel.app/',
+  },
+];
 
 export const GameCarousel = () => {
-  const [mainImage, setMainImage] = useState(img1);
+  const [selectedGameIndex, setSelectedGameIndex] = useState(0);
+  const [mainImage, setMainImage] = useState(games[0].heroImage);
 
-  const games = [
-    {
-      category: 'Top Played',
-      name: 'Stardew Valley',
-      description: 'Play from your browser, earn up to 5$ a day',
-      heroImage: img1,
-      images: [img1, img2, img3, img4],
-    },
-  ];
+  useEffect(() => {
+    setMainImage(games[selectedGameIndex].heroImage);
+  }, [selectedGameIndex]);
+
+  const increaseGame = () => {
+    const newIndex = selectedGameIndex + 1 < games.length ? selectedGameIndex + 1 : 0;
+    setSelectedGameIndex(newIndex);
+  };
+
+  const decreaseGame = () => {
+    const newIndex = selectedGameIndex - 1 > 0 ? selectedGameIndex - 1 : games.length - 1;
+    setSelectedGameIndex(newIndex);
+  };
 
   return (
     <Row className="GameCarousel">
-      {games.map((game) => {
-        <>
-          <Col span={18} className="GameCarouselLeft">
-            <img src={game.heroImage} className="GameImage"></img>
-          </Col>
-          <Col span={6} className="GameCarouselRight">
-            <Text type="secondary">{game.category}</Text>
-            <Title level={2} style={{ margin: `0 0 20px 0` }}>
-              {game.name}
-            </Title>
-            <Row gutter={[16, 24]}>
-              {game.images.map((img, idx) => (
-                <Col span={12} key={idx}>
-                  <img
-                    src={img}
-                    className="GameImage imageThumb"
-                    onMouseOver={() => setMainImage(img)}
-                    onFocus={() => setMainImage(img)}
-                  ></img>
-                </Col>
-              ))}
-            </Row>
-            <Row>
-              <Text style={{ marginTop: 10 }}>{game.description}</Text>
-            </Row>
-            <div className="Action">
-              <Button type="primary">Play Now</Button>
-            </div>
-            <div className="Switch">
-              <Button shape="circle" type="primary" icon={<LeftOutlined />} />
-              <Text>1 of {games.length}</Text>
-              <Button shape="circle" type="primary" icon={<RightOutlined />} />
-            </div>
-          </Col>
-        </>;
-      })}
-
-      <Col span={18} className="GameCarouselLeft">
-        <img src={mainImage} className="GameImage"></img>
-      </Col>
-      <Col span={6} className="GameCarouselRight">
-        <Text type="secondary">Top Played</Text>
-        <Title level={2} style={{ margin: `0 0 20px 0` }}>
-          Stardew Valley
-        </Title>
-        <Row gutter={[16, 24]}>
-          <Col span={12}>
-            <img
-              src={img2}
-              className="GameImage imageThumb"
-              onMouseOver={() => setMainImage(img2)}
-              onFocus={() => setMainImage(img2)}
-            ></img>
-          </Col>
-          <Col span={12}>
-            <img
-              src={img3}
-              className="GameImage imageThumb"
-              onMouseOver={() => setMainImage(img3)}
-              onFocus={() => setMainImage(img3)}
-            ></img>
-          </Col>
-          <Col span={12}>
-            <img
-              src={img4}
-              className="GameImage imageThumb"
-              onMouseOver={() => setMainImage(img4)}
-              onFocus={() => setMainImage(img4)}
-            ></img>
-          </Col>
-          <Col span={12}>
-            <img
-              src={img1}
-              className="GameImage imageThumb"
-              onMouseOver={() => setMainImage(img1)}
-              onFocus={() => setMainImage(img1)}
-            ></img>
-          </Col>
-        </Row>
-        <Row>
-          <Text style={{ marginTop: 10 }}>
-            Play from your browser, earn up to 5$ a day
-          </Text>
-        </Row>
-        <div className="Action">
-          <Button type="primary">Play Now</Button>
-        </div>
-        <div className="Switch">
-          <Button shape="circle" type="primary" icon={<LeftOutlined />} />
-          <Text>1 of 5</Text>
-          <Button shape="circle" type="primary" icon={<RightOutlined />} />
-        </div>
-      </Col>
+      <>
+        <Col span={18} className="GameCarouselLeft">
+          <img src={mainImage} className="GameImage"></img>
+        </Col>
+        <Col span={6} className="GameCarouselRight">
+          <Text type="secondary">{games[selectedGameIndex].category}</Text>
+          <Title level={2} style={{ margin: `0 0 20px 0` }}>
+            {games[selectedGameIndex].name}
+          </Title>
+          <Row gutter={[16, 24]}>
+            {games[selectedGameIndex].images.map((img, idx) => (
+              <Col span={12} key={idx}>
+                <img
+                  src={img}
+                  className="GameImage imageThumb"
+                  onMouseOver={() => setMainImage(img)}
+                  onFocus={() => setMainImage(img)}
+                ></img>
+              </Col>
+            ))}
+          </Row>
+          <Row>
+            <Text style={{ marginTop: 10 }}>{games[selectedGameIndex].description}</Text>
+          </Row>
+          <div className="Action">
+            <a href={games[selectedGameIndex].link} target="_blank" rel="noreferrer">
+              <Button type="primary">Play now</Button>
+            </a>
+          </div>
+          <div className="Switch">
+            <Button
+              shape="circle"
+              type="primary"
+              icon={<LeftOutlined />}
+              onClick={decreaseGame}
+            />
+            <Text>
+              {selectedGameIndex + 1} of {games.length}
+            </Text>
+            <Button
+              shape="circle"
+              type="primary"
+              icon={<RightOutlined />}
+              onClick={increaseGame}
+            />
+          </div>
+        </Col>
+      </>
     </Row>
   );
 };
