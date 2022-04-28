34910e780f215c4f951a89231d2dab27fa9db788
zhunor <ozerorhun@gmail.com>
2022-04-28 21:56:03 +0300

    🥞 Add swap contract

--- src/components/LeftNavigation/index.tsx
+++ src/components/LeftNavigation/index.tsx
@@ -8,12 +8,38 @@ import {
   TableOutlined,
   TrophyOutlined,
 } from '@ant-design/icons';
-import { Button, Card, Input } from 'antd';
+import { contractABI, contractAddress } from 'abi/contract';
+import { Button, Card, InputNumber } from 'antd';
 import { useAccountContext } from 'contexts/accountContext';
-import React from 'react';
+import { ethers } from 'ethers';
+import React, { useState } from 'react';
+import { handleError } from 'utils/common';
 
 export const LeftNavigation = () => {
   const [accountState] = useAccountContext();
+  const [tileAmount, setTileAmount] = useState<number>();
+
+  const swapTileForRose = async () => {
+    try {
+      const { ethereum } = window;
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      if (tileAmount) {
+        let nftTxn = await connectedContract.swapTileForRose(
+          ethers.utils.parseUnits(tileAmount?.toString(), 'ether'),
+        );
+
+        await nftTxn.wait();
+        console.log(nftTxn);
+      }
+    } catch (error: any) {
+      handleError(error);
+    }
+  };
+
   return (
     <div className="LeftNavigation">
       <Card title="Browse Categories" bordered={false}>
@@ -34,16 +60,23 @@ export const LeftNavigation = () => {
         </p>
       </Card>
       <Card title="Exchange ROSE token" bordered={false} className="ExchangeCard">
-        <Input size="large" placeholder="0.0 in ROSE" />
+        <InputNumber
+          type="number"
+          size="large"
+          placeholder="0.0 in TILE"
+          // value={tileAmount}
+          step="0.000000001"
+          onChange={(value) => setTileAmount(Number(value))}
+        />
         <br></br>
         <ArrowDownOutlined />
-        <Input size="large" placeholder="0.0 in Game Token" />
+        <InputNumber size="large" placeholder="0.0 in ROSE" value={tileAmount} disabled />
         <Button
           block
           size="large"
           loading={accountState.isLoading}
           className="button-fancy"
-          onClick={() => {}}
+          onClick={swapTileForRose}
         >
           {accountState.account ? 'Swap' : 'Connect Wallet'}
         </Button>
