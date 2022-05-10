12092012485c068fa274cab3df929714f132e830
zhunor <ozerorhun@gmail.com>
2022-05-10 23:53:52 +0300

    Add swap in both ways for TILE and ROSE tokens

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -28,70 +28,10 @@ export const Footer = () => {
     setRecentAd(publishedAds[publishedAds.length - 1]);
   };
 
-  const mintAdvertiserToken = async () => {
-    try {
-      const { ethereum } = window;
-
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const connectedContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-      const transaction = await connectedContract.buyTile({
-        value: ethers.utils.parseEther('1.0'),
-      });
-
-      const transactionType = 'Buy';
-
-      const transactionID = transaction.hash;
-
-      // I'm confused about this, transaction.to seems to be the address Dominic provided,
-      // and transaction.from is my wallet address, isn't it supposed to be the other way around?
-      // For now, I'm switching them up, please correct any mistakes
-      const from = transaction.to;
-      const to = transaction.from;
-
-      const d = new Date();
-      const date = d.toLocaleDateString() + '-' + d.toLocaleTimeString();
-
-      const amount = 1;
-
-      await transaction.wait();
-
-      insertTransaction(transactionID, amount, transactionType, from, to, date);
-
-      handleSuccess(transaction);
-
-      console.log(transaction);
-    } catch (error) {
-      handleError(error);
-    }
-  };
-
-  async function insertTransaction(
-    transactionID: any,
-    amount: any,
-    transactionType: any,
-    from: any,
-    to: any,
-    date: any,
-  ) {
-    const newTransaction = {
-      transactionId: transactionID,
-      date: date,
-      transactionType: transactionType,
-      from: from,
-      to: to,
-      amount: amount,
-    };
-
-    axios.post('http://localhost:3001/makeTransaction', newTransaction);
-  }
-
   return (
     <div className="Footer">
-      <Button icon={<DollarCircleFilled />} type="primary" onClick={mintAdvertiserToken}>
-        Buy Tile
-      </Button>
+      <Text> </Text>
+
       {recentAd && (
         <div className="oasis-ad" style={{ textAlign: 'center' }}>
           <h3 style={{ fontWeight: 600 }}>{recentAd.advertisementTitle}</h3>
@@ -99,7 +39,6 @@ export const Footer = () => {
             href={recentAd.advertisementUrl}
             title={recentAd.advertisementUrl}
             target="_blank"
-            style={{ margin: '.5em 0 -.7em 0' }}
             className="ant-btn button-fancy"
             rel="noreferrer"
           >
--- frontend/src/components/LeftNavigation/index.tsx
+++ frontend/src/components/LeftNavigation/index.tsx
@@ -8,9 +8,10 @@ import {
 } from '@ant-design/icons';
 import { contractABI, contractAddress, tileTokenAddress, tileABI } from 'abi/contract';
 import { Button, Card, InputNumber } from 'antd';
+import Title from 'antd/lib/typography/Title';
 import { useAccountContext } from 'contexts/accountContext';
 import { ethers } from 'ethers';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { handleError, handleSuccess } from 'utils/common';
 
 import axios from 'axios';
@@ -18,12 +19,7 @@ import axios from 'axios';
 export const LeftNavigation = ({ handleCategoryChoice, chosenCategory }: any) => {
   const [accountState] = useAccountContext();
   const [tileAmount, setTileAmount] = useState<number>();
-
-  /*
-  =========
-  FUNCTIONS
-  =========
-  */
+  const [direction, setDirection] = useState<'TR' | 'RT'>('TR');
 
   async function insertTransaction(
     transactionID: any,
@@ -66,6 +62,7 @@ export const LeftNavigation = ({ handleCategoryChoice, chosenCategory }: any) =>
       // I'm confused about this, transaction.to seems to be the address Dominic provided,
       // and transaction.from is my wallet address, isn't it supposed to be the other way around?
       // For now, I'm switching them up, please correct any mistakes
+
       const from = transaction.to;
       const to = transaction.from;
 
@@ -79,8 +76,6 @@ export const LeftNavigation = ({ handleCategoryChoice, chosenCategory }: any) =>
       insertTransaction(transactionID, amount, transactionType, from, to, date);
 
       handleSuccess(transaction);
-
-      console.log(transaction);
     } catch (error: any) {
       handleError(error);
     }
@@ -93,11 +88,6 @@ export const LeftNavigation = ({ handleCategoryChoice, chosenCategory }: any) =>
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
       const connectedContract = new ethers.Contract(contractAddress, contractABI, signer);
-      // console.log(connectedContract);
-      // await connectedContract.approve(
-      //   contractAddress,
-      //   ethers.utils.parseUnits('0.0001', 'ether'),
-      // );
 
       if (tileAmount) {
         let transaction = await connectedContract.swapTileForRose(
@@ -124,7 +114,6 @@ export const LeftNavigation = ({ handleCategoryChoice, chosenCategory }: any) =>
         insertTransaction(transactionID, amount, transactionType, from, to, date);
 
         handleSuccess(transaction);
-        console.log(transaction);
       }
     } catch (error: any) {
       handleError(error);
@@ -147,10 +136,64 @@ export const LeftNavigation = ({ handleCategoryChoice, chosenCategory }: any) =>
 
         await transaction.wait();
         handleSuccess(transaction);
-        console.log(transaction);
       }
     } catch (error) {
       handleError(error);
+    }
+  };
+
+  const exchange = useMemo(
+    () => ({
+      from: direction === 'RT' ? 'ROSE' : 'TILE',
+      to: direction === 'RT' ? 'TILE' : 'ROSE',
+    }),
+    [direction],
+  );
+
+  const changeDirection = () => {
+    direction === 'TR' ? setDirection('RT') : setDirection('TR');
+  };
+
+  const swapRoseForTile = async () => {
+    try {
+      const { ethereum } = window;
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      if (tileAmount) {
+        const transaction = await connectedContract.buyTile({
+          value: ethers.utils.parseEther(tileAmount.toString()),
+        });
+
+        const transactionType = 'Buy';
+
+        const transactionID = transaction.hash;
+
+        const from = transaction.to;
+        const to = transaction.from;
+        const d = new Date();
+        const date = d.toLocaleDateString() + '-' + d.toLocaleTimeString();
+        const amount = 1;
+
+        await transaction.wait();
+
+        insertTransaction(transactionID, amount, transactionType, from, to, date);
+
+        handleSuccess(transaction);
+      }
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  const swap = async () => {
+    if (direction === 'TR') {
+      await approve();
+      await swapTileForRose();
+    } else {
+      await swapRoseForTile();
     }
   };
 
@@ -191,32 +234,47 @@ export const LeftNavigation = ({ handleCategoryChoice, chosenCategory }: any) =>
         </button>
       </Card>
       <Card title="Exchange ROSE token" bordered={false} className="ExchangeCard">
-        <InputNumber
-          type="number"
-          size="large"
-          placeholder="0.0 in TILE"
-          // value={tileAmount}
-          step="0.1"
-          onChange={(value) => setTileAmount(Number(value))}
-        />
+        <div>
+          <Title level={4} style={{ textAlign: 'left' }}>
+            {exchange.from}
+          </Title>
+          <InputNumber
+            type="number"
+            size="large"
+            placeholder={`0.0 in ${exchange.from}`}
+            step="0.1"
+            onChange={(value) => setTileAmount(Number(value))}
+          />
+        </div>
+
         <br></br>
-        <ArrowDownOutlined />
-        <InputNumber
-          size="large"
-          placeholder="0.0 in ROSE"
-          value={tileAmount}
-          disabled
-          step={0.1}
+
+        <Button
+          type="default"
+          shape="circle"
+          icon={<ArrowDownOutlined />}
+          size={`large`}
+          onClick={changeDirection}
         />
+
+        <div>
+          <Title level={4} style={{ textAlign: 'left' }}>
+            {exchange.to}
+          </Title>
+          <InputNumber
+            size="large"
+            placeholder={`0.0 in ${exchange.to}`}
+            value={tileAmount}
+            disabled
+          />
+        </div>
+
         <Button
           block
           size="large"
           loading={accountState.isLoading}
           className="button-fancy"
-          onClick={async () => {
-            await approve();
-            await swapTileForRose();
-          }}
+          onClick={swap}
         >
           {accountState.account ? 'Swap' : 'Connect Wallet'}
         </Button>
